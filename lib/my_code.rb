0148b4413ff4f694d1e048a451c3589605ea17b6
Conrad Robinson <robinsonc220@gmail.com>
2019-10-24 01:58:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,7 @@ end
 def reduce_to_all_true(source_array)
   counter = 0
   while counter < source_array.size do
-    return false if source_array#???????
+    return false if source_array[counter] == false
     counter +=1
   end
   return true
